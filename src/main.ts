2d1c57b6f1b6db0906dba63ec46317496937ee7d
valkineles <valkineles@gmail.com>
2020-03-22 18:43:10 -0300

    add firebase-admin

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,17 @@ import * as admin from 'firebase-admin';
 
 import { AppModule } from './app.module';
 
-/* eslint-disable @typescript-eslint/no-var-requires */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const service = require('./firebase-adminsdk.json');
+
   admin.initializeApp({
     credential: admin.credential.cert(service),
     databaseURL: process.env.DATABASE_URL_FIREBASE,
   });
-  const port: number = +process.env.PORT || 3000;
+  const port: number = +process.env.PORT || 3300;
   await app.listen(port, () => {
     console.log(`Servidor executando na porta ${port}`);
   });
